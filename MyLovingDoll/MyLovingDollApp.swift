d54109021e047fddee3f43c1c98e3708e99cd137
Fever Sun <feversun@outlook.com>
2025-10-29 15:12:54 +0800

    Initial Commit

---
+++ MyLovingDoll/MyLovingDollApp.swift
@@ -0,0 +1,32 @@
+//
+//  MyLovingDollApp.swift
+//  MyLovingDoll
+//
+//  Created by How Sun on 2025/10/29.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MyLovingDollApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ MyLovingDollTests/MyLovingDollTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyLovingDollTests.swift
+//  MyLovingDollTests
+//
+//  Created by How Sun on 2025/10/29.
+//
+
+import Testing
+@testable import MyLovingDoll
+
+struct MyLovingDollTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
