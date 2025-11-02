f10058c2837d8c088f76efd7b85f79023a242fbd
Fever Sun <feversun@outlook.com>
2025-11-02 21:11:56 +0800

    ADD: Copy customizable disintegration effect to project
    - Added customizable DisintegrationEffect.swift for project-specific particle tuning
    - Added View+Snapshot.swift extension to support the effect
    - Updated SubjectExtractionView to use customDisintegrationEffect()
    - Parameters marked for easy adjustment: particle count, scatter range, blur radius
    - PhotoEffectsKit generic version remains unchanged

--- MyLovingDoll/Views/Components/SubjectExtractionView.swift
+++ MyLovingDoll/Views/Components/SubjectExtractionView.swift
@@ -37,7 +37,7 @@ struct SubjectExtractionView: View {
                             Image(uiImage: cropped)
                                 .resizable()
                                 .aspectRatio(contentMode: .fit)
-                                .disintegrationEffect(isDeleted: triggerDisintegration) {
+                                .customDisintegrationEffect(isDeleted: triggerDisintegration) {
                                     // 动画完成后放大主体
                                     withAnimation(.spring(response: 0.6, dampingFraction: 0.7)) {
                                         showSubject = true
